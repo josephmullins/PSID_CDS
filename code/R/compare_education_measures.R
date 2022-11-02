f59f65c345501ea3a771250c9203d7d7fe69d859
josephmullins <josephlyonmullins@gmail.com>
2022-11-02 14:58:32 -0500

    education
    as before

--- code/R/compare_education_measures.R
+++ code/R/compare_education_measures.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 
-ed2uc = read.csv("~/Dropbox/PSID_CDS/PSID_RAW/HGC.csv") %>%
+educ = read.csv("~/Dropbox/PSID_CDS/PSID_RAW/HGC.csv") %>%
   mutate(X=NULL,MID=intnum68*1000+pnum68)
 
 educ2 = read.csv("~/Dropbox/Data/PSID_MAIN/Education/grades_completed.csv") %>%
@@ -27,16 +27,23 @@ D1 <- D[,seq(5,158,4)]
 
 # this routine pulls out the most recent education variable
 # we want the age at which it's measured, too?
-D$educ2 <- -1
-D$year_meas <- -1
+D$educ2 <- 0
+D$year_meas <- 0
 years <- c(1968,seq(1970,1996),seq(1997,2017,2))
 
 for (i in 1:36) {
   print(i)
   I_use <- (D1[,i]>0) & (D1[,i]<98)
   D[I_use,"educ2"] <- D1[I_use,i]
-  #D[I_use,"year_meas"] <- years[i]
+  D[I_use,"year_meas"] <- years[i]
 }
+
+# now the next block verifies that this replicates the numbers from HGC.csv:
+D %>%
+  rename(MID = ID) %>%
+  select(MID,educ2) %>%
+  merge(educ) %>%
+  filter(hgc!=educ2)
 
 D %>%
   rename(MID = ID) %>%
