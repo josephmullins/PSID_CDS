db1acc41e3325fadba338d8aac099541c281434c
Joseph Mullins <josephlyonmullins@gmail.com>
2024-10-22 10:13:33 -0500

    #cleaned up scripts

--- code/R/clean-scripts/make_foodstamps.R
+++ code/R/clean-scripts/make_foodstamps.R
@@ -1,7 +1,7 @@
 # This script loads the data on household childcare expenditures from the main interview and arranges it into a panel.
 # To merge the data with individuals, you need to use the identifiers panel.
 
-D <- readxl::read_excel("~/Dropbox/PSID_CDS/data-main/food-stamps/food-stamps.xlsx")
+D <- readxl::read_excel("data-main/food-stamps/food-stamps.xlsx")
 
 years <- c(seq(1976,1997),seq(1999,2021,2))
 
@@ -71,74 +71,6 @@ for (i in 1:length(years)) {
 }
 
 
-
-##--------TESTING
-
-D <- readxl::read_excel("~/Dropbox/PSID_CDS/data-main/food-stamps/food-stamps.xlsx")
-
-
-#V10001  	#RELEASE NUMBER
-#V10002  	#1984 INTERVIEW NUMBER
-#V10241  	# MOS USED FD ST 83(J22)
-
-D<-D %>% 
-  select(V10001, V10002, V10241)
-
-
-years <- c(seq(1976,1997),seq(1999,2021,2))
-
-
-years<-c(1984)
-
-d <- data.frame(year = integer(), intnum = integer(), fs = integer(),fs_used=integer())
-
-question_count_original <- 3
-
-for (i in 1:length(years)) {
-  
-  question_count_original <- 3
-  question_count <- ifelse(years[i] <= 1997, 3, 4)
-  
-  if (years[i] <= 1997) {
-    col_indices <- (i - 1) * question_count_original + 1 + (0:(question_count - 1))
-    j=0
-  } else {
-    col_indices <- (i - 1) * question_count_original + 1 + ((0:(question_count - 1))+j)
-    j=j+1
-  }
-  col_indices
-  
-  d0 <- data.frame(
-    year = years[i],
-    intnum = D[, col_indices[2]],  
-    fs = D[, tail(col_indices,1)] #after 1997, it's the 4th question that tracks the prior year and the 3rd question that tracks 2 years ago
-  )
-  
-  # Rename columns to match specified names
-  colnames(d0) <- c("year", "intnum", "fs")
-  
-  if(years[i]<1994){
-    d0$fs_used[d0$fs > 0 & d0$fs <= 12] <- 1
-    d0$fs_used[d0$fs == 0] <- 0
-    d0$fs_used[d0$fs == 99] <- NA
-    d0 <- d0[!is.na(d0$intnum), ] #drop NA interview numbers
-    d <- rbind(d, na.omit(d0))
-  }
-  
-  #if(years[i]>=1994){
-  #  d0$fs_used <- NA  # Initialize all values to NA
-  #  d0$fs_used[d0$fs == 1] <- 1  # Set values equal to 1
-  #  d0$fs_used[d0$fs == 5] <- 0  # Set values equal to 5
-  #  d0 <- d0[!is.na(d0$intnum), ] #drop NA interview numbers
-  #  d <- rbind(d, na.omit(d0))
-  #}
-  
-}
-
-
-
-
-
 snap_use <- d %>%
   group_by(year) %>%
   summarize(
@@ -155,13 +87,4 @@ ggplot(snap_use, aes(x = year, y = fs_participation)) +
   theme_minimal()
 
 
-
-
-
-
-snap_use_hold<-snap_use
-
-
-
-
-write.csv(d,"/Users/madisonbozich/Dropbox/PSID_CDS/data-main/food-stamps/food-stamps.csv")
+write.csv(d,"data-main/food-stamps/food-stamps.csv")
--- code/R/clean-scripts/make_welfare.R
+++ code/R/clean-scripts/make_welfare.R
@@ -1,7 +1,7 @@
 # This script loads the data on household childcare expenditures from the main interview and arranges it into a panel.
 # To merge the data with individuals, you need to use the identifiers panel.
 
-D <- readxl::read_excel("/Users/madisonbozich/Dropbox/PSID_CDS/data-main/afdc/afdc.xlsx")
+D <- readxl::read_excel("data-main/afdc/afdc.xlsx")
 years <- c(seq(1986,1997),seq(1999,2021,2))
 d <- data.frame(year = integer(), intnum = integer(), afdc_hd = integer(), afdc_sp = integer(), afdc_hh = integer())
 
@@ -45,7 +45,7 @@ for (i in 1:length(years)) {
     d <- rbind(d, na.omit(d0))
   }
   
-  if(years[i]>=1994){
+  if(years[i]>=1994) {
     d0$afdc_hd[d0$afdc_hd == 1] <- 1
     d0$afdc_hd[d0$afdc_hd == 5] <- 0
     d0$afdc_sp[d0$afdc_sp == 1] <- 1
@@ -57,8 +57,6 @@ for (i in 1:length(years)) {
     d0 <- d0[!is.na(d0$intnum), ]
     d <- rbind(d, na.omit(d0))
   }
-  
-  # Handle skipped years after 1999
   if (years[i] > 1997 & years[i] < 2017 & i < length(years)) {
     skipped_year <- years[i] - 1
     # Construct entry for skipped year
@@ -79,105 +77,13 @@ for (i in 1:length(years)) {
     # Append to the main data frame
     d <- rbind(d, d_skipped)
   }
-  
-}
-
-
-welfare_use <- d %>%
-  group_by(year) %>%
-  summarize(
-    afdc_hh = sum(afdc_hh == 1, na.rm = TRUE) / sum(!is.na(afdc_hh))
-  )
-print(welfare_use,n=33)
-
-
-welfare_use_check <- afdc %>%
-  group_by(year) %>%
-  summarize(
-    afdc_hh = sum(afdc_hh == 1, na.rm = TRUE) / sum(!is.na(afdc_hh))
-  )
-print(welfare_use_check,n=33)
-
-
-
-####welfare_use --- testing  
-##########-----MORE TEStING
-
-D <- readxl::read_excel("/Users/madisonbozich/Dropbox/PSID_CDS/data-main/afdc/afdc.xlsx")
-years <- c(seq(1986,1997),seq(1999,2021,2))
-d <- data.frame(year = integer(), intnum = integer(), afdc_hd = integer(), afdc_sp = integer(), afdc_hh = integer())
-
-#------TESTING
-
-for (i in 1:length(years)) {
-  # Count the number of questions asked each year
-  
-  question_count_original <- 4
-  question_count <- ifelse(years[i] <= 1997 | years[i] > 2015, 4, 5)
-  
-  
-  if (years[i] <= 1997) {
-    col_indices <- (i - 1) * question_count_original + 1 + (0:(question_count - 1))
-    j = 0
-  } else if (years[i] > 1997 & years[i] < 2017) {
-    col_indices <- (i - 1) * question_count_original + 1 + ((0:(question_count - 1)) + j)
-    j = j + 1
-  } else {
-    k=9
-    col_indices <- (i - 1) * question_count_original + 1 + (0:(question_count - 1)+k)
-    k=k+1
-  }
-  
-  d0 <- data.frame(
-    year = years[i],
-    intnum = D[, col_indices[2]],  # Start indexing at 2
-    afdc_hd = D[, col_indices[3]],
-    afdc_sp = D[, col_indices[4]]
-  )
-  
-  # Rename columns to match specified names
-  colnames(d0) <- c("year", "intnum", "afdc_hd", "afdc_sp")
-  
-  d0 <- d0 %>%
-    filter(!is.na(intnum))
-  
-  if(years[i]<1994){
-    d0$afdc_hd[d0$afdc_hd > 0] <- 1
-    d0$afdc_sp[d0$afdc_sp > 0] <- 1
-    d0$afdc_hh <- ifelse(d0$afdc_sp == 1 | d0$afdc_hd == 1, 1, NA) #should maintain NAs
-    d0$afdc_hh <- ifelse(d0$afdc_sp == 0 & d0$afdc_hd == 0, 0, d0$afdc_hh)
-    d0 <- d0[!is.na(d0$intnum), ]
-    d <- rbind(d, na.omit(d0))
-  }
-  
-  
-  #6396,2759
-  
-  #if(years[i]>=1994){
-  #  d0$afdc_hd[d0$afdc_hd == 1] <- 1
-  #  d0$afdc_hd[d0$afdc_hd == 5] <- 0
-  #  d0$afdc_sp[d0$afdc_sp == 1] <- 1
-  #  d0$afdc_sp[d0$afdc_sp == 5] <- 0
-    
-  #  d0$afdc_hh <- ifelse(d0$afdc_sp == 1 | d0$afdc_hd == 1, 1, NA) #should maintain NAs
-  #  d0$afdc_hh <- ifelse(d0$afdc_sp == 0 & d0$afdc_hd == 0, 0, d0$afdc_hh)
-    
-  #  d0 <- d0[!is.na(d0$intnum), ]
-  #  d <- rbind(d, na.omit(d0))
-  #}
-  
 
 }
 
 
 
-
-
-
-
-
 welfare_use <- d %>%
-group_by(year) %>%
+  group_by(year) %>%
   summarize(
     afdc_hh = sum(afdc_hh == 1, na.rm = TRUE) / sum(!is.na(afdc_hh))
   )
@@ -190,4 +96,4 @@ ggplot(welfare_use, aes(x = year, y = afdc_hh)) +
   theme_minimal()
 
 
-write.csv(d,"/Users/madisonbozich/Dropbox/PSID_CDS/data-main/afdc/afdc.csv")
+write.csv(d,"data-main/afdc/afdc.csv")
